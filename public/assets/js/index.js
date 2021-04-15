052189042eaf854dab3baa9403321103aefa1fa2
Tarek Behairy <tbtarek@gmail.com>
2021-04-15 00:19:54 -0400

    updated CSS and js file for front end

---
+++ public/assets/js/index.js
@@ -0,0 +1,27 @@
+$(".devour-btn").click(function (e) {
+  e.preventDefault();
+
+  const id = $(this).attr("data-id");
+
+  $.ajax(
+    {
+      url: `/api/burgers/${id}`,
+      method: "PUT",
+      data: { devoured: 1 }
+    })
+    .then(response => location.reload());
+});
+
+$(".submit-btn").click((e) => {
+  e.preventDefault();
+
+  const name = $("#burger-name").val().trim();
+
+  $.ajax(
+    {
+      url: "/api/burgers",
+      method: "POST",
+      data: { burger_name: name }
+    })
+    .then(response => location.reload());
+});
