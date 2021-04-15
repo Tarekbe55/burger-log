6400b2c6eb53a8fc938ab4c932e7c978c416e89a
Tarek Behairy <tbtarek@gmail.com>
2021-04-15 00:22:05 -0400

    updated controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,17 +3,14 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-/* GET to return index with all burger records */
 router.get("/", (req, res) => {
-  /* Pull all burgers from DB and send to index */
   burger.all(results =>
     res.render("index", { burgers: results })
   );
 });
 
-/* POST to add a new burger */
+
 router.post("/api/burgers", (req, res) => {
-  /* Create new burger with provided name */
   burger.create({ burger_name: req.body.burger_name }, results => {
     if (results.affectedRows === 0) {
       return res.status(500).end();
@@ -22,9 +19,7 @@ router.post("/api/burgers", (req, res) => {
   });
 });
 
-/* PUT to update an existing burger */
 router.put("/api/burgers/:id", (req, res) => {
-  /* Update burger data */
   burger.update(req.body, req.params.id, results => {
 
     if (results.affectedRows === 0) {
